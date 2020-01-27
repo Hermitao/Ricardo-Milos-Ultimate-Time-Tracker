83533a2d39572b166294081c4b50248ba3593d3c
Hermitao <45779281+Hermitao@users.noreply.github.com>
2020-01-27 19:42:10 -0300

    Update Ricardo Milos Ultimate Time Tracker.py

--- Ricardo Milos Ultimate Time Tracker.py
+++ Ricardo Milos Ultimate Time Tracker.py
@@ -1,6 +1,6 @@
 '''MIT License
 
-Copyright (c) 2020 Vinicius Krieger Granemann
+Copyright (c) 2020 Hermitao
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -35,7 +35,7 @@ from pygame import mixer
 
 mixer.init()
 mixer.music.load('thesong.ogg')
-mixer.music.play()
+mixer.music.play(-1)
 
 clear = lambda: os.system('cls')
 
