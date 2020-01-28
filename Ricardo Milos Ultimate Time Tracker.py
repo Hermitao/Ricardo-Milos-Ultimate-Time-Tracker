b1b9412185f18fe96cd3729bbc6cba0b9df9c30d
Hermitao <45779281+Hermitao@users.noreply.github.com>
2020-01-27 21:41:23 -0300

    Add files via upload

--- Ricardo Milos Ultimate Time Tracker.py
+++ Ricardo Milos Ultimate Time Tracker.py
@@ -1,6 +1,6 @@
 '''MIT License
 
-Copyright (c) 2020 Hermitao
+Copyright (c) 2020 Vinicius Krieger Granemann
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -115,25 +115,25 @@ sys.stdout.write("""
                                               ...,,,,,,,,,,,,,,,,,.....,,,,,,,,,*****************%&**///////((#((((//******////////((((((((
 
 
-                                       __ _                   _                _ _               
-                                      /__(_) ___ __ _ _ __ __| | ___     /\/\ (_) | ___  ___     
-                                     / \// |/ __/ _` | '__/ _` |/ _ \   /    \| | |/ _ \/ __|    
-                                    / _  \ | (_| (_| | | | (_| | (_) | / /\/\ \ | | (_) \__ \    
-                                    \/ \_/_|\___\__,_|_|  \__,_|\___/  \/    \/_|_|\___/|___/    
+       __ _                   _                _ _               
+      /__(_) ___ __ _ _ __ __| | ___     /\/\ (_) | ___  ___     
+     / \// |/ __/ _` | '__/ _` |/ _ \   /    \| | |/ _ \/ __|    
+    / _  \ | (_| (_| | | | (_| | (_) | / /\/\ \ | | (_) \__ \    
+    \/ \_/_|\___\__,_|_|  \__,_|\___/  \/    \/_|_|\___/|___/    
+                                                                                                                                             
+                     __ __________          _   _____  __                
+             /\ /\  / //__   \_   \/\/\    /_\ /__   \/__\               
+            / / \ \/ /   / /\// /\/    \  //_\\  / /\/_\                 
+            \ \_/ / /___/ //\/ /_/ /\/\ \/  _  \/ / //__                 
+             \___/\____/\/ \____/\/    \/\_/ \_/\/  \__/                 
+                                                                                                                     
+                         _____ _                  _____                _             
+                        /__   (_)_ __ ___   ___  /__   \_ __ __ _  ___| | _____ _ __ 
+                          / /\/ | '_ ` _ \ / _ \   / /\/ '__/ _` |/ __| |/ / _ \ '__|
+                         / /  | | | | | | |  __/  / /  | | | (_| | (__|   <  __/ |   
+                         \/   |_|_| |_| |_|\___|  \/   |_|  \__,_|\___|_|\_\___|_|   
                                                                                                          
-                                                             __ __________          _   _____  __                
-                                                     /\ /\  / //__   \_   \/\/\    /_\ /__   \/__\               
-                                                    / / \ \/ /   / /\// /\/    \  //_\\  / /\/_\                 
-                                                    \ \_/ / /___/ //\/ /_/ /\/\ \/  _  \/ / //__                 
-                                                     \___/\____/\/ \____/\/    \/\_/ \_/\/  \__/                 
-                                                                                                         
-                                                                 _____ _                  _____                _             
-                                                                /__   (_)_ __ ___   ___  /__   \_ __ __ _  ___| | _____ _ __ 
-                                                                  / /\/ | '_ ` _ \ / _ \   / /\/ '__/ _` |/ __| |/ / _ \ '__|
-                                                                 / /  | | | | | | |  __/  / /  | | | (_| | (__|   <  __/ |   
-                                                                 \/   |_|_| |_| |_|\___|  \/   |_|  \__,_|\___|_|\_\___|_|   
-                                                                                                         
-
+                                By Hermitao
 
 
 """)
@@ -144,7 +144,7 @@ projectsFileContents = None
 try:
     with open("Projects.xml", "r+") as projectsFile:
         projectsFileContents = projectsFile.readlines()
-except FileNotFoundError:
+except IOError:
     with open("Projects.xml", "w+") as projectsFile:
         projectsFileContents = projectsFile.readlines()
 
@@ -170,7 +170,7 @@ if ((myInput + "\n") not in projectsFileContents):
 try:
     with open(myInput + ".xml", "r+") as f:
         contents = f.read()
-except FileNotFoundError:
+except IOError:
     with open(myInput + ".xml", "w+") as f:
         contents = f.read()
 
